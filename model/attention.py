487156459711603934fe79da4ccf2e565febd194
albonkey <43227255+albonkey@users.noreply.github.com>
2024-03-23 13:40:29 -0700

    Updated model & attention

--- model/attention.py
+++ model/attention.py
@@ -1,3 +1,11 @@
+import trax
+from trax import layers
+from trax.fastmath import numpy as jnp
+
 # Implement Dot Product Attention
+def DotProductAttention(query, key, value, mask=None):
+  pass
 
 # Implement Causal Attention
+def CausalAttention(dimensionOfFeature, numberOfHeads, mode='train'):
+  pass
--- model/model.py
+++ model/model.py
@@ -1,3 +1,37 @@
+import trax
+from trax import layers as tl
+from trax.fastmath import numpy as jnp
+
 # Implement Decoder Block
+def DecoderBlock(model_depth, ff_depth, ff_activation, nr_heads, dropout, mode):
+  pass
 
 # Implement TransformerLM
+def TransformerLM(
+  vocab_size=33300,
+  model_depth=512,
+  ff_depth=2048,
+  ff_activation=tl.Relu,
+  nr_layers=6,
+  nr_heads=8,
+  dropout=0.1,
+  max_length=4096,
+  mode='train',
+):
+  positional_encoder = [
+    tl.Embedding(vocab_size, model_depth),
+    tl.Dropout(rate=dropout, mode=mode),
+    tl.PositionalEncoding(max_length=max_length, mode=mode),
+  ]
+  decoder_blocks = [
+    DecoderBlock(model_depth, ff_depth, ff_activation, nr_heads, dropout, mode) for _ in range(nr_layers)
+  ]
+
+  return tl.Serial(
+    tl.ShiftRight(mode=mode),
+    positional_encoder,
+    decoder_blocks,
+    tl.LayerNorm(),
+    tl.Dense(vocab_size),
+    tl.LogSoftmax()
+  )
